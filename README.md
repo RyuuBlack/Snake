02486f1b7f543caeedf66aa1886233d01034b424
Yerson Velandia <ryuublack_yavl@gmail.com>
2024-01-19 18:21:07 -0500

    Se agrego código inicial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Snake
+
+
+Este programa es un juego parecido a la culebrita de Nokia
+
+---
+
+## Hecho en C++
+
---
+++ snake.cpp
@@ -0,0 +1,17 @@
+#iclude <iostream>
+
+using namespace std;
+
+void gotoxy(int x, int y){
+
+
+}
+
+int main(){
+
+
+
+
+  return 0;
+}
+
